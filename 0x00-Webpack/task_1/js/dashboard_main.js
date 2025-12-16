c0afa0f0cd0885aa5771118bd7c99fcd692ef1e2
Bassel-saleh <basselh26@gmail.com>
2025-12-16 20:08:01 +0200

    debug#8 task#1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
+import _ from 'lodash';
 
 $('body').append('<p>ALX Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -17,4 +17,4 @@ function updateCounter() {
   $('#count').html(`${count} clicks on the button`);
 };
 
-button.on('click', debounce(updateCounter, 500));
+button.on('click', _.debounce(updateCounter, 500));
